ae8f6b042832cd6191dc4bfbd07206efeb71baff
Steve <stevenzybertusn@gmail.com>
2021-12-19 16:56:37 -0500

    Initial Commit
    Start of Alien Biomes!
    - new other-worldly biomes
    - Enlightened Plains
    - 2 new plants
    - new terrain defs

---
+++ 1.3/Source/Alien Biomes/Alien Biomes/NightBlooming_ModExtension.cs
@@ -0,0 +1,12 @@
+﻿using Verse;
+
+namespace Alien_Biomes
+{
+    public class NightBlooming_ModExtension : DefModExtension
+    {
+        public string nightfallGraphicPath;
+        //public float nightBloomStart = 0.80f;
+        //public float nightBloomEnd = 0.25f;
+        //From 0.00 - 1.00, this value being a % of time of day.
+    }
+}
---
+++ 1.3/Source/Alien Biomes/Alien Biomes/Plant_NightBlooming.cs
@@ -0,0 +1,54 @@
+﻿using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace Alien_Biomes
+{
+    [StaticConstructorOnStartup]
+    public class Plant_NightBlooming : Plant
+    {
+        public static Graphic GraphicSowing = GraphicDatabase.Get<Graphic_Single>("Things/Plant/Plant_Sowing", ShaderDatabase.Cutout, Vector2.one, Color.white);
+        public string nightfallGraphicPath;
+        private Graphic graphicNightFallCache;
+        public Graphic GraphicNightFall => graphicNightFallCache;
+        // no loading GraphicNightFall every frame
+
+        public override Graphic Graphic
+        {
+            get
+            {
+                if (LifeStage == PlantLifeStage.Sowing)
+                {
+                    return GraphicSowing;
+                }
+
+                if (!def.HasModExtension<NightBlooming_ModExtension>())
+                {
+                    return base.Graphic;
+                }
+
+                NightBlooming_ModExtension plantExt = def.GetModExtension<NightBlooming_ModExtension>();
+                if ((GenLocalDate.DayPercent(this) * Rand.RangeSeeded(0.9f, 1.1f, Position.x)) >= 0.75 || (GenLocalDate.DayPercent(this) * Rand.RangeSeeded(0.9f, 1.1f, Position.x)) <= 0.20)
+                    // This statement checks time of day on the player map (GenLocalDate.DayPercent), in sections (Rand.RangeSeeded)
+                    // DayPercent max = 1.00 or 24 hours, so 0.75 = 18 hours
+                    // ...so here, the check is for the hours of 1800 - 0448
+                {
+                    //Log.Message("Swap Texture Check " + GenLocalDate.DayPercent(this));
+                    return graphicNightFallCache ??= GraphicDatabase.Get<Graphic_Single>(plantExt.nightfallGraphicPath, ShaderDatabase.TransparentPlant, Vector2.one, Color.white);
+                }
+
+                if (def.plant.leaflessGraphic != null && LeaflessNow && (!sown || !HarvestableNow))
+                {
+                    return def.plant.leaflessGraphic;
+                }
+
+                if (def.plant.immatureGraphic != null && !HarvestableNow)
+                {
+                    return def.plant.immatureGraphic;
+                }
+
+                return base.Graphic;
+            }
+        }
+    }
+}
