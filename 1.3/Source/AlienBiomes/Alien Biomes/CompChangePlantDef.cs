5f21c5d7dee343a3fedcf2c12b232750a26ff041
Steve <stevenzybertusn@gmail.com>
2021-12-23 08:34:52 -0500

    changes lul

---
+++ 1.3/Source/AlienBiomes/Alien Biomes/CompChangePlantDef.cs
@@ -0,0 +1,34 @@
+﻿using RimWorld;
+using Verse;
+
+namespace AlienBiomes
+{
+    public class CompChangePlantDef : ThingComp
+    {
+        // This comp changes a def to another def based on the time of day (GenLocalDate.DayPercent).
+        // 
+        public CompProperties_ChangePlantDef Props => (CompProperties_ChangePlantDef)props;
+        public override void CompTickLong()
+        {
+            var dayPercent = GenLocalDate.DayPercent(this.parent.Map);
+
+            if ((this.parent.Spawned) && (dayPercent >= Props.__duskTime || dayPercent <= Props.__dawnTime))
+            {
+
+                if (this.parent.Position != null)
+                {
+                    Log.Warning("null position" + this.parent.Position);
+
+                    if (this.parent.Map != null)
+                    {
+                        Log.Warning("null Map" + this.parent.Map);
+
+                        ThingDef thingToSpawn = ThingDef.Named(Props.__defToChangeTo);
+                        GenSpawn.Spawn(thingToSpawn, this.parent.Position, this.parent.Map, WipeMode.Vanish);
+                        this.parent.Destroy();
+                    }
+                }
+            }
+        }
+    }
+}
--- 1.3/Source/AlienBiomes/Alien Biomes/CompProperties_ChangePlantDef.cs
+++ 1.3/Source/AlienBiomes/Alien Biomes/CompProperties_ChangePlantDef.cs
@@ -4,7 +4,9 @@ namespace AlienBiomes
 {
     public class CompProperties_ChangePlantDef : CompProperties
     {
-        public string defToChangeTo = "";
+        public string __defToChangeTo = ""; // New DefName to change to.
+        public float? __duskTime = null; // Time window when def changes at sundown.
+        public float? __dawnTime = null; // Time window when def changes at sunrise.
 
         public CompProperties_ChangePlantDef()
         {
