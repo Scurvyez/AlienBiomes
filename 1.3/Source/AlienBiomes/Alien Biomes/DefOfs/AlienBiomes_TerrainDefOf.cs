1bccf93818488ee53d937247c39cdd46299a524b
Steve <stevenzybertusn@gmail.com>
2022-01-15 14:34:31 -0500

    more shit
    new textures, new code, slight xml fixes, the usual

---
+++ 1.3/Source/AlienBiomes/Alien Biomes/DefOfs/AlienBiomes_TerrainDefOf.cs
@@ -0,0 +1,17 @@
+﻿using Verse;
+using RimWorld;
+
+namespace AlienBiomes
+{
+    [DefOf]
+    public static class AlienBiomes_TerrainDefOf
+    {
+        public static TerrainDef SZ_EnlightenedSoil;
+        public static TerrainDef SZ_EnlightenedRichSoil;
+        public static TerrainDef SZ_EnlightenedMud;
+        public static TerrainDef SZ_EnlightenedStonySoil;
+        public static TerrainDef SZ_SoothingSand;
+
+        static AlienBiomes_TerrainDefOf() => DefOfHelper.EnsureInitializedInCtor(typeof(TerrainDefOf));
+    }
+}
---
+++ 1.3/Source/AlienBiomes/Alien Biomes/Harmony/AlienBiomes_SteamGeyserSpawnSetup_Patch.cs
@@ -0,0 +1,28 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+using UnityEngine;
+using System.Collections.Generic;
+using System;
+
+namespace AlienBiomes
+{
+    [HarmonyPatch(typeof(Building_SteamGeyser), "SpawnSetup")]
+    public static class AlienBiomes_SteamGeyserSpawnSetup_Patch
+    {
+        [HarmonyPostfix]
+        public static void SpawnSetupUpdateGeysers(Map map, bool respawningAfterLoad, Building_SteamGeyser __instance)
+        {
+            var steamGeyserOne = ThingDef.Named("SZ_SteamGeyserEnlightenedSoil");
+            var steamGeyserTwo = ThingDef.Named("SZ_SteamGeyserEnlightenedRichSoil");
+
+            // checks to see if the given terrain at the vanilla geysers' pos is "enlightened soil"
+            if (map.terrainGrid.TerrainAt(__instance.Position) == AlienBiomes_TerrainDefOf.SZ_EnlightenedSoil)
+            {
+                // spawn new steam geyser
+                Thing thing = ThingMaker.MakeThing(steamGeyserOne);
+                GenPlace.TryPlaceThing(thing, __instance.Position, map, ThingPlaceMode.Direct);
+            }
+        }
+    }
+}
