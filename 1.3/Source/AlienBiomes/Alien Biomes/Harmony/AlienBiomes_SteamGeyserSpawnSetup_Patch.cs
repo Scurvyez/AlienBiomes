dac0fc7464da612446c2586029aca063026068ca
Steve <stevenzybertusn@gmail.com>
2022-01-16 14:25:06 -0500

    Name changes & Geysers
    Completed the vanilla geyser replacement code and textures.
    Renamed some files containing "Enlightened" to "Radiant".
    Updated biome patch makers to include SoothingSand instead of StonySoil.

--- 1.3/Source/AlienBiomes/Alien Biomes/Harmony/AlienBiomes_SteamGeyserSpawnSetup_Patch.cs
+++ 1.3/Source/AlienBiomes/Alien Biomes/Harmony/AlienBiomes_SteamGeyserSpawnSetup_Patch.cs
@@ -7,13 +7,14 @@ using System;
 
 namespace AlienBiomes
 {
+    // This patch replaces vanilla steam geyser textures on map gen.
+
     [HarmonyPatch(typeof(Building_SteamGeyser), "SpawnSetup")]
     public static class AlienBiomes_SteamGeyserSpawnSetup_Patch
     {
         [HarmonyPostfix]
         public static void SpawnSetupUpdateGeysers(Map map, bool respawningAfterLoad, Building_SteamGeyser __instance)
         {
-
             var steamGeyserOne = ThingDef.Named("SZ_SteamGeyserRadiantSoil");
             var steamGeyserTwo = ThingDef.Named("SZ_SteamGeyserRadiantRichSoil");
             var vanillaGeyserPos = __instance.Position;
@@ -23,14 +24,33 @@ namespace AlienBiomes
             {
                 if (!(__instance is Building_SteamGeyserRadiantSoil))
                 {
+                    // Allows for geysers to be destroyed.
+                    Thing.allowDestroyNonDestroyable = true;
                     if (!__instance.Destroyed)
                     {
                         // Destroy old geyser.
                         __instance.Destroy(DestroyMode.Vanish);
                         // Spawn new geyser, in same cell.
-                        Thing newGeyser = ThingMaker.MakeThing(steamGeyserOne);
-                        GenPlace.TryPlaceThing(newGeyser, vanillaGeyserPos, map, ThingPlaceMode.Direct);
+                        Thing newGeyserOne = ThingMaker.MakeThing(steamGeyserOne);
+                        GenPlace.TryPlaceThing(newGeyserOne, vanillaGeyserPos, map, ThingPlaceMode.Direct);
                     }
+                    // Disallows geyser destroying.
+                    Thing.allowDestroyNonDestroyable = false;
+                }
+            }
+
+            else if (map.terrainGrid.TerrainAt(vanillaGeyserPos) == AlienBiomes_TerrainDefOf.SZ_RadiantRichSoil)
+            {
+                if (!(__instance is Building_SteamGeyserRadiantRichSoil))
+                {
+                    Thing.allowDestroyNonDestroyable = true;
+                    if (!__instance.Destroyed)
+                    {
+                        __instance.Destroy(DestroyMode.Vanish);
+                        Thing newGeyserTwo = ThingMaker.MakeThing(steamGeyserTwo);
+                        GenPlace.TryPlaceThing(newGeyserTwo, vanillaGeyserPos, map, ThingPlaceMode.Direct);
+                    }
+                    Thing.allowDestroyNonDestroyable = false;
                 }
             }
         }
