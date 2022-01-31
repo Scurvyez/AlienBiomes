b956c2a306f35bf1ca77169ce1532338215247cd
Steve <stevenzybertusn@gmail.com>
2022-01-30 20:25:07 -0500

    plant fleck emitter code
    Added a mod to-do list as well.

---
+++ 1.3/Source/AlienBiomes/Alien Biomes/Comps/CompProperties/CompProperties_PlantFleckEmitter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Verse;
+
+namespace AlienBiomes
+{
+    public class CompProperties_PlantFleckEmitter : CompProperties
+    {
+        public FleckDef fleck;
+        public Vector3 offset;
+        public FloatRange scale;
+        public FloatRange rotationRate;
+        public int burstCount = 1;
+        public SoundDef soundOnEmission = null;
+        public Color colorA = Color.white;
+        public Color colorB = Color.white;
+        public FloatRange velocityX;
+        public FloatRange velocityY;
+
+        public CompProperties_PlantFleckEmitter() => compClass = typeof(Comp_PlantFleckEmitter);
+
+        public override IEnumerable<string> ConfigErrors(ThingDef parentDef)
+        {
+            if (fleck == null)
+                yield return "Comp_PlantFleckEmitter must have a fleck assigned.";
+        }
+    }
+}
---
+++ 1.3/Source/AlienBiomes/Alien Biomes/Comps/Comp_PlantFleckEmitter.cs
@@ -0,0 +1,51 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using UnityEngine;
+using Verse;
+
+namespace AlienBiomes
+{
+    public class Comp_PlantFleckEmitter : ThingComp
+    {
+        private CompProperties_PlantFleckEmitter Props => (CompProperties_PlantFleckEmitter)props;
+
+        private Color EmissionColor => Color.Lerp(Props.colorA, Props.colorB, Rand.Value);
+
+        public override void CompTickRare()
+        {
+            List<Pawn> pawnsTouchingPlants = new();
+
+            if (parent.Spawned)
+            {
+                List<Thing> pawnList = parent.Position.GetThingList(parent.Map);
+                for (int i = 0; i < pawnList.Count; ++i)
+                {
+                    if (pawnList[i] is Pawn p1 && !pawnsTouchingPlants.Contains(p1))
+                    {
+                        pawnsTouchingPlants.Add(p1);
+                        Emit();
+                    }
+                }
+                for (int ii = 0; ii < pawnsTouchingPlants.Count; ++ii)
+                {
+                    Pawn touchingPawn = pawnsTouchingPlants[ii];
+                    if (!touchingPawn.Spawned || touchingPawn.Position != parent.Position)
+                        pawnsTouchingPlants.Remove(touchingPawn);
+                }
+            }
+        }
+        
+        private void Emit()
+        {
+            for (int iii = 0; iii < Props.burstCount; ++iii)
+            {
+                FleckCreationData dataStatic = FleckMaker.GetDataStatic(parent.DrawPos, parent.Map, Props.fleck, Props.scale.RandomInRange);
+                dataStatic.rotationRate = Props.rotationRate.RandomInRange;
+                dataStatic.instanceColor = new Color?(EmissionColor);
+                dataStatic.velocityAngle = Props.velocityX.RandomInRange;
+                dataStatic.velocitySpeed = Props.velocityY.RandomInRange;
+                parent.Map.flecks.CreateFleck(dataStatic);
+            }
+        }
+    }
+}
