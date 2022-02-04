8f27bea8c8819132c61c1b2f55fbf74f7a690218
Steve <stevenzybertusn@gmail.com>
2022-02-03 22:30:14 -0500

    Comments

--- 1.3/Source/AlienBiomes/Alien Biomes/Comps/Comp_PlantFleckEmitter.cs
+++ 1.3/Source/AlienBiomes/Alien Biomes/Comps/Comp_PlantFleckEmitter.cs
@@ -13,23 +13,29 @@ namespace AlienBiomes
         private CompProperties_PlantFleckEmitter Props => (CompProperties_PlantFleckEmitter)props;
 
         private Color EmissionColor => Color.Lerp(Props.colorA, Props.colorB, Rand.Value);
+        // New variable color for the fleck itself.
         public List<Pawn> pawnsTouchingPlants = new();
+        // A list of all pawns on the map when touching plants.
 
         public override void CompTick()
         {
             List<Thing> pawnList = parent.Position.GetThingList(parent.Map);
+            // List of parent position(s).
             for (int i = 0; i < pawnsTouchingPlants.Count; ++i)
+                // Loop to remove pawns from list of those ouching plant(s).
             {
                 Pawn touchingPawn = pawnsTouchingPlants[i];
                 if (!touchingPawn.Spawned || touchingPawn.Position != parent.Position)
                     pawnsTouchingPlants.Remove(touchingPawn);
             }
             for (int ii = 0; ii < pawnList.Count; ++ii)
+                // Loop to add pawns to list of those touching plant(s).
             {
                 if (pawnList[ii] is Pawn p1 && !pawnsTouchingPlants.Contains(p1))
                 {
                     pawnsTouchingPlants.Add(p1);
                     Emit();
+                    // Call Emit and cast the fleck. :)
                     //Log.Message("Fleck was emitted at " + parent.Position);
                 }
             }
@@ -40,6 +46,7 @@ namespace AlienBiomes
         {
             SoundDef soundDefUsed = Props.soundOnEmission;
             for (int iii = 0; iii < Props.burstCount; ++iii)
+                // Fleck and sound creation loop.
             {
                 FleckCreationData dataStatic = FleckMaker.GetDataStatic(parent.DrawPos, parent.Map, Props.fleck, Props.scale.RandomInRange);
                 dataStatic.rotationRate = Props.rotationRate.RandomInRange;
