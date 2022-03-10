0ecfb715ae1146e21fd299251b9aeb95b08e3f2a
Steve <stevenzybertusn@gmail.com>
2022-03-09 22:21:46 -0500

    more shit
    "changes..."

--- 1.3/Source/AlienBiomes/Alien Biomes/DefModExtensions/Geyser_GraphicPerTerrain.cs
+++ 1.3/Source/AlienBiomes/Alien Biomes/DefModExtensions/Geyser_GraphicPerTerrain.cs
@@ -1,37 +1,40 @@
 ﻿using System.Collections.Generic;
 using Verse;
 using UnityEngine;
+using RimWorld;
 
 namespace AlienBiomes
 {
     public class Geyser_GraphicPerTerrain : DefModExtension
     {
         public List<TerrainDef> terrains;
-        public Graphic[] newGraphics;
-        public List<string> newGraphicPaths;
+        public Graphic[] graphics;
+        public List<string> graphicPaths;
         bool initialized = false;
         
         public void Initialize()
         {
+            //Log.Warning("INITIALIZED");
             var count = terrains.Count;
-            newGraphics = new Graphic[count];
+            graphics = new Graphic[count];
 
             for (int i = 0; i < count; i++)
             {
-                if (newGraphicPaths[i] != null)
-                    newGraphics[i] = GraphicDatabase.Get(typeof(Graphic_Single), newGraphicPaths[i], ShaderDatabase.Transparent, Vector2.one, Color.white, Color.white);
+                if (graphicPaths[i] != null)
+                    graphics[i] = GraphicDatabase.Get(typeof(Graphic_Single), graphicPaths[i], ShaderDatabase.Transparent, Vector2.one, Color.white, Color.white);
             }
             initialized = true;
         }
 
-        public Graphic NewGeyserGraphic(TerrainDef geyser)
+        public Graphic GeyserGraphicPerTerrain(TerrainDef terrain)
         {
+            //Log.Warning("FOUND TERRAIN: " + terrain);
             if (!initialized)
                 Initialize();
-            var geyserIndex = terrains.IndexOf(geyser);
-            if (geyserIndex == -1 || newGraphics[geyserIndex] == null)
+            var terrainIndex = terrains.IndexOf(terrain);
+            if (terrainIndex == -1 || graphics[terrainIndex] == null)
                 return null;
-            return newGraphics[geyserIndex];
+            return graphics[terrainIndex];
         }
     }
 }
