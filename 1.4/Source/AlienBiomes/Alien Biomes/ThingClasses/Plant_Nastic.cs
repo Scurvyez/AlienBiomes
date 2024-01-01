278c94ec8e8bf9ff86b0e6397999cf99843d544d
Steve <84108749+Scurvyez@users.noreply.github.com>
2024-01-01 02:13:59 -0500

    :)
    random graphics fixed, partially

--- 1.4/Source/AlienBiomes/Alien Biomes/ThingClasses/Plant_Nastic.cs
+++ 1.4/Source/AlienBiomes/Alien Biomes/ThingClasses/Plant_Nastic.cs
@@ -1,6 +1,7 @@
 ﻿using RimWorld;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using Verse;
 
@@ -13,18 +14,22 @@ namespace AlienBiomes
         public float CurrentScale = 1f;
         public int TouchSensitiveStartTime;
         public Color FleckEmissionColor;
-        private List<Vector3> InstanceOffsets = new();
-        private float EasedTime;
+        private List<Vector3> InstanceOffsets = new ();
         private const int MaxTicks = 720;
+        private float CurPlantGrowth;
+        protected Graphic cachedGraphic;
 
-        public float[] scaleDeltaCache = null;
-        
         public override void SpawnSetup(Map map, bool respawningAfterLoad)
         {
             base.SpawnSetup(map, respawningAfterLoad);
             plantExt = def.GetModExtension<Plant_Nastic_ModExtension>();
 
+            overrideGraphicIndex = thingIDNumber;
+
+            if (Graphic is Graphic_Random random) overrideGraphicIndex %= random.SubGraphicsCount;
+
             InitializeRandomOffsets();
+            PrecomputeScaleDeltas();
             IntVec3[] cells = GenRadial.RadialCellsAround(Position, plantExt.effectRadius, useCenter: true).ToArray();
             MapComponent_PlantGetter plantGetter = map.GetComponent<MapComponent_PlantGetter>();
             foreach (IntVec3 cell in cells)
@@ -54,33 +59,53 @@ namespace AlienBiomes
         public override void Tick()
         {
             base.Tick();
+            CurPlantGrowth = def.plant.visualSizeRange.LerpThroughRange(Growth);
+
             if (plantExt != null)
             {
                 int timeSinceLastStep = Find.TickManager.TicksGame - TouchSensitiveStartTime;
                 if (timeSinceLastStep < MaxTicks)
                 {
-                    EasedTime = (float)timeSinceLastStep / 360f;
-
-                    // Use linear interpolation for scaleDeltaDecrease (no easing)
-                    float scaleDeltaDecreaseEased = EasedTime;
-                    float scaleChangeRateDecrease = Mathf.Lerp(-plantExt.scaleDeltaDecrease, 0f, scaleDeltaDecreaseEased);
-
-                    // Use InOutCubic for scaleDeltaIncrease (slow and smooth)
-                    float scaleDeltaIncreaseEased = ABEasingFunctions.EaseOutQuad(EasedTime);
-                    float scaleChangeRateIncrease = Mathf.Lerp(0f, plantExt.scaleDeltaIncrease, scaleDeltaIncreaseEased);
-
-                    float scaleChangeRate = scaleChangeRateDecrease + scaleChangeRateIncrease;
+                    float scaleChangeRate = plantExt.scaleDeltaCache[timeSinceLastStep];
                     CurrentScale = Mathf.Clamp(CurrentScale + scaleChangeRate, plantExt.minScale, 1);
                 }
             }
         }
 
+        public void ChangeGraphic()
+        {
+            overrideGraphicIndex++;
+            if (Graphic is Graphic_Random random) overrideGraphicIndex %= random.SubGraphicsCount;
+            ClearCache();
+        }
+
+        protected void ClearCache()
+        {
+            cachedGraphic = null;
+        }
+
+        public override Graphic Graphic
+        {
+            get
+            {
+                if (cachedGraphic == null)
+                {
+                    Graphic graphic = def.graphicData.GraphicColoredFor(this);
+                    if (graphic is Graphic_Random random)
+                    {
+                        cachedGraphic = random.SubGraphicFor(this);
+                    }
+                    else
+                    {
+                        cachedGraphic = graphic;
+                    }
+                }
+                return cachedGraphic;
+            }
+        }
+
         public override void DrawAt(Vector3 drawLoc, bool flip = false)
         {
-            Graphic extraGraphic = Graphic;
-            Material material = extraGraphic.MatSingle;
-            float pGrowth = def.plant.visualSizeRange.LerpThroughRange(Growth);
-
             for (int i = 0; i < InstanceOffsets.Count; i++)
             {
                 // Draw the mesh with the modified UV coordinates
@@ -91,8 +116,8 @@ namespace AlienBiomes
                 float scaleY = Mathf.Lerp(-1f, 0.5f, CurrentScale);
                 drawPos.z += def.graphicData.drawSize.y * scaleY / 2f;
 
-                Matrix4x4 matrix = Matrix4x4.TRS(drawPos, Rotation.AsQuat, new Vector3(CurrentScale * pGrowth, 1, CurrentScale * pGrowth));
-                Graphics.DrawMesh(MeshPool.plane10, matrix, material, 0, null, 0, null, false, false, false);
+                Matrix4x4 matrix = Matrix4x4.TRS(drawPos, Rotation.AsQuat, new Vector3(CurrentScale * CurPlantGrowth, 1, CurrentScale * CurPlantGrowth));
+                Graphics.DrawMesh(MeshPool.plane10, matrix, Graphic.MatSingle, 0, null, 0, null, false, false, false);
             }
         }
 
@@ -108,6 +133,29 @@ namespace AlienBiomes
                 instancePos.z += zOffset;
 
                 InstanceOffsets.Add(instancePos);
+            }
+        }
+
+        private void PrecomputeScaleDeltas()
+        {
+            if (plantExt != null)
+            {
+                int cycleTicks = MaxTicks; // Assuming one full cycle
+                plantExt.scaleDeltaCache = new float[cycleTicks];
+
+                for (int i = 0; i < cycleTicks; i++)
+                {
+                    float easedTime = (float)i / 360f;
+
+                    float scaleDeltaDecreaseEased = easedTime;
+                    float scaleChangeRateDecrease = Mathf.Lerp(-plantExt.scaleDeltaDecrease, 0f, scaleDeltaDecreaseEased);
+
+                    float scaleDeltaIncreaseEased = ABEasingFunctions.EaseOutQuad(easedTime);
+                    float scaleChangeRateIncrease = Mathf.Lerp(0f, plantExt.scaleDeltaIncrease, scaleDeltaIncreaseEased);
+
+                    float scaleChangeRate = scaleChangeRateDecrease + scaleChangeRateIncrease;
+                    plantExt.scaleDeltaCache[i] = scaleChangeRate;
+                }
             }
         }
 
