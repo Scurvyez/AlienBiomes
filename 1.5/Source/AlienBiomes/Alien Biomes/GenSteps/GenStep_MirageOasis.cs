5495dd7f9ddf97a47afa99609874ecb2a0b79706
Steve <84108749+Scurvyez@users.noreply.github.com>
2024-03-26 20:32:12 -0400

    another small dunes update

--- 1.5/Source/AlienBiomes/Alien Biomes/GenSteps/GenStep_MirageOasis.cs
+++ 1.5/Source/AlienBiomes/Alien Biomes/GenSteps/GenStep_MirageOasis.cs
@@ -2,7 +2,6 @@
 using UnityEngine;
 using Verse;
 using RimWorld;
-using Verse.Noise;
 
 namespace AlienBiomes
 {
@@ -15,43 +14,30 @@ namespace AlienBiomes
     {
         public override int SeedPart => 931457342;
         public TerrainDef spawnOnTerDef = null;
-        public TerrainDef outerWaterDef = null;
-        public TerrainDef outerSandDef = null;
+        public TerrainDef waterDef = null;
+        public TerrainDef sandDef = null;
         public List<ThingDef> plantsToGen = new();
         public float plantGenChance = 1f;
         public float plantGenRadius = 10f;
-        public List<IntRange> waterRadiusOne = new();
-        public List<IntRange> waterRadiusTwo = new();
-        public IntRange outerSandRadius = new();
+        public List<IntRange> waterRadius = new();
+        public IntRange sandRadius = new();
         public List<IntRange> mapSizeRadiusAdjust = new();
 
         private HashSet<IntVec3> waterCells = new HashSet<IntVec3>();
 
         public override void Generate(Map map, GenStepParams parms)
         {
+            waterCells.Clear();
             if (map.Biome == ABDefOf.SZ_DeliriousDunes)
             {
-                IntVec3 center1 = ValidCentralSpawnCell(map);
-                if (center1.IsValid)
+                IntVec3 center = ValidCentralSpawnCell(map);
+                if (center.IsValid)
                 {
                     int mapAdjustmentFactor = AdjustedRadiusByMapSize(map, mapSizeRadiusAdjust).RandomInRange;
-                    int waterRadiusOneFinal = AdjustedRadiusByMapSize(map, waterRadiusOne).RandomInRange + mapAdjustmentFactor;
-                    int waterRadiusTwoFinal = AdjustedRadiusByMapSize(map, waterRadiusTwo).RandomInRange + mapAdjustmentFactor;
-
-                    Log.Message($"Map adjustment factor: <color=#ff8c66>{mapAdjustmentFactor}</color>");
-                    Log.Message($"Final water 1 radius: <color=#ff8c66>{waterRadiusOneFinal}</color>");
-                    Log.Message($"Final water 2 radius: <color=#ff8c66>{waterRadiusTwoFinal}</color>");
-
-                    IntVec3 center2 = FindSecondPatchCenter(map, center1, waterRadiusOneFinal);
-                    if (center2.IsValid)
-                    {
-                        GeneratePatch(map, center1, waterRadiusOneFinal);
-                        //GeneratePatch(map, center2, waterRadiusTwoFinal);
-                        GenerateThings(map, center1);
-                        //GenerateThings(map, center2);
-
-                        Log.Message($"Total oasis water cells: <color=#ff8c66>{waterCells.Count}</color>");
-                    }
+                    int waterRadiusFinal = AdjustedRadiusByMapSize(map, waterRadius).RandomInRange + mapAdjustmentFactor;
+                    GenerateWaterPatch(map, center, waterRadiusFinal);
+                    GenerateSandAroundWater(map);
+                    GenerateThings(map, center);
                 }
             }
         }
@@ -62,29 +48,28 @@ namespace AlienBiomes
             return mapSizeIndex < range.Count ? range[mapSizeIndex] : new IntRange(1, 1);
         }
 
-        private void GeneratePatch(Map map, IntVec3 center, int waterRadius)
+        private void GenerateWaterPatch(Map map, IntVec3 center, int radius)
         {
-            // Generate water terrain (our first pass for the entire oasis)
-            foreach (IntVec3 cell in GenRadial.RadialCellsAround(center, waterRadius, true))
+            foreach (IntVec3 cell in GenRadial.RadialCellsAround(center, radius, true))
             {
                 if (cell.GetTerrain(map) == spawnOnTerDef)
                 {
-                    map.terrainGrid.SetTerrain(cell, outerWaterDef);
+                    map.terrainGrid.SetTerrain(cell, waterDef);
                     waterCells.Add(cell);
                 }
             }
+        }
 
-            // Generate sand terrain around the waters' edge
+        private void GenerateSandAroundWater(Map map)
+        {
             foreach (IntVec3 waterCell in waterCells)
             {
-                int outerSandRadiusFinal = outerSandRadius.RandomInRange;
-                Log.Message($"Final sand radius for {waterCell}: <color=#ff8c66>{outerSandRadiusFinal}</color>");
-
-                foreach (IntVec3 cell in GenRadial.RadialCellsAround(center, outerSandRadiusFinal, true))
+                int sandRadiusFinal = sandRadius.RandomInRange;
+                foreach (IntVec3 cell in GenRadial.RadialCellsAround(waterCell, sandRadiusFinal, true))
                 {
-                    if (cell.InBounds(map) && cell.GetTerrain(map) != outerWaterDef)
+                    if (cell.InBounds(map) && cell.GetTerrain(map) != waterDef)
                     {
-                        map.terrainGrid.SetTerrain(cell, outerSandDef);
+                        map.terrainGrid.SetTerrain(cell, sandDef);
                     }
                 }
             }
@@ -93,10 +78,9 @@ namespace AlienBiomes
         private void GenerateThings(Map map, IntVec3 center)
         {
             HashSet<IntVec3> usedCells = new HashSet<IntVec3>();
-
             foreach (IntVec3 cell in GenRadial.RadialCellsAround(center, plantGenRadius, true))
             {
-                if (cell.GetTerrain(map) == outerSandDef && cell.Standable(map) && !usedCells.Contains(cell))
+                if (cell.GetTerrain(map) == sandDef && cell.Standable(map) && !usedCells.Contains(cell))
                 {
                     if (plantsToGen != null && Rand.Chance(plantGenChance))
                     {
@@ -105,7 +89,6 @@ namespace AlienBiomes
                         plant.Growth = Rand.Value;
                         GenSpawn.Spawn(plant, cell, map);
 
-                        // Mark the cell as used
                         usedCells.Add(cell);
                     }
                 }
@@ -123,30 +106,6 @@ namespace AlienBiomes
                 cell.Standable(map) &&
                 !cell.Fogged(map) &&
                 cell.GetTerrain(map) == spawnOnTerDef, map, 1000);
-        }
-
-        /// <summary>
-        /// Try to find a second patch center within the water radius of the first patch center.
-        /// </summary>
-        private IntVec3 FindSecondPatchCenter(Map map, IntVec3 firstPatchCenter, int waterRadius)
-        {
-            IntVec3 center = IntVec3.Invalid;
-
-            for (int i = 0; i < 10; i++)
-            {
-                IntVec3 candidate = CellFinderLoose.RandomCellWith(cell =>
-                    cell.InBounds(map) &&
-                    cell.DistanceTo(firstPatchCenter) <= waterRadius &&
-                    cell.Standable(map) &&
-                    !cell.Fogged(map), map, 100);
-
-                if (candidate.IsValid)
-                {
-                    center = candidate;
-                    break;
-                }
-            }
-            return center;
         }
     }
 }
